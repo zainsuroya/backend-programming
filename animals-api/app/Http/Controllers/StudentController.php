c6d306c288921e8fb6a97e173c0faaa87d401bfc
zainsuroya <nuni19007ti@student.nurulfikri.ac.id>
2021-11-06 09:13:51 +0700

    commit task5

---
+++ animals-api/app/Http/Controllers/StudentController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+        $data = [
+            'message' => 'Get all students',
+            'data' => $students
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $input = [
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+        ];
+        $student = Student::create($input);
+        $data = [
+            'message' => 'Student is created succesfully',
+            'data' => $student,
+        ];
+
+        return response()->json($data, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            $input = [
+                'nama' => $request->nama ?? $student->nama,
+                'nim' => $request->nim ?? $student->nim,
+                'email' => $request->email ?? $student->email,
+                'jurusan' => $request->jurusan ?? $student->jurusan
+            ];
+            $student->update($input);
+            $data = [
+                'message' => 'Student is updated',
+                'data' => $student
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found'
+            ];
+            return response()->json($data, 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
+            $data = [
+                'message' => 'Student is deleted'
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found'
+            ];
+            return response()->json($data, 404);
+        }
+    }
+}
--- animals-api/routes/api.php
+++ animals-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,14 +20,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-# method get
+# Route animals
 Route::get('/animals', [AnimalController::class, 'index']);
-
-# method post
 Route::post('/animals', [AnimalController::class, 'store']);
-
-# method put
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
-
-# method delete
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+# Method GET, route /students
+Route::get('/students', [StudentController::class, 'index']);
+
+# Method POST, route /students
+Route::post('/students', [StudentController::class, 'store']);
+
+# Method PUT, route /students
+Route::put('/students/{id}', [StudentController::class, 'update']);
+
+# Method DELETE, route /students
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
